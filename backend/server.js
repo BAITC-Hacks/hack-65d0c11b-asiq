7f4fb9a467c0162af2374639664f89d05a234693
Asiqpai <trapbuckz@gmail.com>
2026-09-23 14:55:28 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json({ limit: "1mb" }));
-
+app.use(express.static("public"));
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
